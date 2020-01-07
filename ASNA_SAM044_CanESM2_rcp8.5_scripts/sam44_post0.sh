b51c1ec9a8bbfeff58a6fc667676ffe83edb051a
Yoselita <milovacj@unican.es>
2020-01-07 17:13:42 +0100

    Cosmetic changes: Loading paths to the working directories from the external file called dirs included. Some names of the working directories changed.

--- ASNA_SAM044_CanESM2_rcp8.5_scripts/sam44_post0.sh
+++ ASNA_SAM044_CanESM2_rcp8.5_scripts/sam44_post0.sh
@@ -6,53 +6,43 @@
 #SBATCH --ntasks-per-node=16
 #SBATCH --time=72:00:00
 
-#Para filtrar las variables a mano:
+echo "I'm running in ${HOSTNAME}, and today is $(date)"
 
-set -e
 export PATH="/home/uc15/uc15004/miniconda3/bin:$PATH"
-source activate py_interp_2.7
-module load netcdf_tools
+source activate wrfncxnj_py2.7
+source ./dirs
 
-year_block=(2098) 
+
+year_blocks=(2006) #2002 2006 2010 2014 2018 2022 2026 2030 2034 2038 2042 2046 2050) 
 
 for ij in "${year_block[@]}"
   do
   echo $ij
-BASEDIR="/home/uc15/uc15004/projects/EXPERIMENTS/josipa/SAM044_CanESM2_rcp8.5/CanESM2_rcp85"
-DATEDIR="CanESM2_rcp85-default-${ij}0101T000000"
-GEOFILEDIR="/gpfs/res_projects/uc15/DOMAINS/CORDEX_SouthAm044_WRF3.4.1"
-SCRIPTDIR="${BASEDIR}/scripts"
-FULLFILE="${BASEDIR}/scripts"
-DOMAIN=1
-TMPDIR="${SCRIPTDIR}/tmpdir"
-INPUTPOST0="${BASEDIR}/data/CanESM2_rcp85/${DATEDIR}/output"
-OUTPUTPOST0="${BASEDIR}/data/CanESM2_rcp85/${DATEDIR}/post0/"
-INPUTPOST1="${OUTPUTPOST0}"
-ONESTEPPOST1="${OUTPUTPOST0}/outputs_one_step/"
-OUTPUTPOST1="${BASEDIR}/post_data/post_fullres/${DATEDIR}/"
-SINGLERECPOST1="${OUTPUTPOST1}/single_records/"
-INPUTPOST2="${OUTPUTPOST1}"
-OUTPUTPOST2="${BASEDIR}/post_data/post_CORDEX"
 
-dir_scripts=${SCRIPTDIR}
-in_path=${INPUTPOST0}
-out_path=${OUTPUTPOST0}
+REALIZATION="${WRF4G_EXP}-${ij}0101T000000"
+GEOFILEDIR=${GEOFILEDIR}
+INPUTPOST0="${BASEDIR}/data/CanESM2_rcp85/${REALIZATION}/output" #path to the folder with wrfout files, directly from the model
+INPUTPOST0_XTRM="${BASEDIR}/data/CanESM2_rcp85/${REALIZATION}/post0/WRFXTRM/XTRM_MONTHLY" #path to the folder with wrfxtrm files, organised
+OUTPUTPOST0="${BASEDIR}/data/CanESM2_rcp85/${REALIZATION}/post0" #path to the folder where output files after post0 will be written
 
-echo "Input is $in_path"
-echo "Output is $out_path"
-mkdir -p ${out_path}
+indir=${INPUTPOST0}
+outdir=${OUTPUTPOST0}
 
-cd ${INPUTPOST0}
+echo "Input is $indir"
+echo "Output is $outdir"
+mkdir -p ${outdir}
+
+cd ${indir}
 files=$(ls wrfout* -1)
 for wrfnc_file in ${files}
 do
-  ${dir_scripts}/postprocessor.CORDEX_ALL $wrfnc_file
-  mv $wrfnc_file ${out_path}
+  ${SCRIPTDIR}/postprocessor.CORDEX_ALL $wrfnc_file
+  mv $wrfnc_file ${outdir}
   echo "Moving file to post0"
   echo $(free -m | grep Mem)
 done
 
-mv ${in_path}/wrfxtrm* ${out_path}
+mv ${indir}/wrfxtrm* ${outdir}
 echo "Moving wrfxtrm files to post0"
 
 done
