2873766b5a908269e9162aca825c92be5fee9fea
zequihg50 <ezequiel.cimadevilla@unican.es>
2021-03-22 18:17:19 +0100

    Improved store.py

--- store.py
+++ store.py
@@ -5,9 +5,10 @@ import tables
 def parse_args(argv):
     args = {
         'col_size': 1000,
-        'columns': ['id'],
+        'columns': None,
         'dest': None,
         'expectedrows': 10_000_000,
+        'from': sys.stdin,
         'indexes': [],
         'table_name': 'files',
     }
@@ -28,6 +29,23 @@ def parse_args(argv):
         elif argv[pos] == '--expectedrows':
             args['expectedrows'] = int(argv[pos+1])
             pos+=2
+        elif argv[pos] == '-f' or argv[pos] == '--from':
+            args['from'] = argv[pos+1]
+            pos+=2
+        elif argv[pos] == '-h' or argv[pos] == '--help':
+            print('''
+store.py [OPTIONS] DEST
+
+Store JSON objects from esgf-search into PyTables.
+
+Options:
+    --col-size N                Number of characters of the String column.
+    --columns COLUMNS           Comma separated names for columns, default is to use JSON keys.
+    --expectedrows N            Default is 10_000_000.
+    --from FILE                 Read from FILE instead of stdin.
+    --indexes INDEXES           Comma separated columns where indexes will be created.
+    --table-name NAME           HDF5 name of the object where the Table will be created, default is '/files'.''', file=sys.stderr)
+            sys.exit(1)
         elif argv[pos] == '--indexes':
             args['indexes'] = argv[pos+1].split(',')
             pos+=2
@@ -40,19 +58,45 @@ def parse_args(argv):
 
     return args
 
+def read(f):
+    if f is not sys.stdin:
+        fh = open(f, 'r')
+    else:
+        fh = sys.stdin
+
+    for line in fh:
+        yield line.rstrip('\n')
+
+    fh.close()
+
+def append(item, row, columns):
+    for c in columns:
+        row[c] = item[c]
+    row.append()
+
 def main(f, args):
-    schema = dict(zip(args['columns'], [tables.StringCol(args['col_size'])]*len(args['columns'])))
+    # Get table columns from first json object
+    gen = read(args['from'])
+    first = json.loads( next(gen) )
+    columns = first.keys()
+    nkeys = len(columns)
+    schema = dict(zip(columns, [tables.StringCol(args['col_size'])]*nkeys))
+
+    # If columns passed as arguments, override
+    if args['columns'] is not None:
+        columns = args['columns']
+        schema = dict(zip(args['columns'], [tables.StringCol(args['col_size'])]*len(args['columns'])))
+
+    # Create PyTable's table
     filt = tables.Filters(complevel=1, shuffle=True)
     table = f.create_table(f.root, args['table_name'], schema, args['table_name'], filters=filt, expectedrows=args['expectedrows'])
     
-    # Populate table
+    # Populate table, need to insert element obtained before
     row = table.row
-    for line in sys.stdin:
-        d = json.loads(line.rstrip('\n'))
-        for c in args['columns']:
-            if c in d:
-                row[c] = d[c]
-        row.append()
+    append(first, row, columns)
+    for line in gen:
+        d = json.loads(line)
+        append(d, row, columns)
     
     table.flush()
     
